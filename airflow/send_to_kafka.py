be8a291dbf09d9dd467b67c6060cca819e8256cc
natchy <natchjirawat@gmail.com>
2024-05-10 11:25:18 +0700

    👀 remove unused

--- airflow/send_to_kafka.py
+++
@@ -1,8 +0,0 @@
-import json
-from kafka import KafkaProducer
-
-def send_to_kafka(article_data):
-    producer = KafkaProducer(bootstrap_servers='kafka1:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-    for article in article_data:
-        producer.send('test-topic', value=article)
-    producer.flush()
